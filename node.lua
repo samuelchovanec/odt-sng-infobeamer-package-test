d19f6b4c9ea02929fe6b9e6797414d3ff7cc45ab
samuelchovanec <samuel.chovanec@gmail.com>
2023-05-24 10:37:40 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -3,5 +3,5 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 local font = resource.load_font "font.ttf"
 
 function node.render()
-    font:write(1000, 500, "Hello world", 64, 1,1,1,1)
+    font:write(1000, 500, "Hello world", 64, 255,255,255,100)
 end
